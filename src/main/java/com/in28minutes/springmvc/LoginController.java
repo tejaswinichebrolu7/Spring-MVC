518f18339011d2907240db32165cfef4d21fc664
ERICITINHYD\tcstech <tejaswinichebrolu5@gmail.com>
2017-10-27 18:10:09 +0530

    login sample(dispatcher servlet, viewresolver, requestparam, modelmap)

---
+++ src/main/java/com/in28minutes/springmvc/LoginController.java
@@ -0,0 +1,50 @@
+package com.in28minutes.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+	
+	/*@RequestMapping(value = "/login")
+	public String sayHello(){
+		return "Hello World dummy!";
+	}*/
+	
+	/*@RequestMapping(value = "/login")
+	@ResponseBody
+	public String sayHello(){
+		return "Hello World dummy!";
+	}*/
+	
+	
+	/*@RequestMapping(value = "/login")
+	@ResponseBody
+	public String sayHello(){
+		return "login";
+	}*/
+
+	//to redirect to view resolver
+	/*@RequestMapping(value = "/login")
+	public String sayHello(){
+		return "login";
+	}*/
+	
+	//to handle only GET Request
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String sayHello(){
+		return "login";
+	}
+	
+	//to handle only POST Request
+		@RequestMapping(value = "/login", method = RequestMethod.POST)
+		public String handleLoginRequest(@RequestParam String name,@RequestParam String password, ModelMap model){
+			model.put("name", name);
+			model.put("password", password);
+			return "welcome";
+		}
+}
