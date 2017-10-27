65a7c75af446b20dc38f54a82567821119ca77ff
ERICITINHYD\tcstech <tejaswinichebrolu5@gmail.com>
2017-10-27 18:33:56 +0530

    @Service, @Autowired annotations

---
+++ src/main/java/com/in28minutes/login/LoginController.java
@@ -0,0 +1,60 @@
+package com.in28minutes.login;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.in28minutes.login.LoginService;
+
+@Controller
+public class LoginController {
+	
+	@Autowired
+	LoginService service;
+	
+	/*@RequestMapping(value = "/login")
+	public String sayHello(){
+		return "Hello World dummy!";
+	}*/
+	
+	/*@RequestMapping(value = "/login")
+	@ResponseBody
+	public String sayHello(){
+		return "Hello World dummy!";
+	}*/
+	
+	
+	/*@RequestMapping(value = "/login")
+	@ResponseBody
+	public String sayHello(){
+		return "login";
+	}*/
+
+	//to redirect to view resolver
+	/*@RequestMapping(value = "/login")
+	public String sayHello(){
+		return "login";
+	}*/
+	
+	//to handle only GET Request
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String sayHello(){
+		return "login";
+	}
+	
+	//to handle only POST Request
+		@RequestMapping(value = "/login", method = RequestMethod.POST)
+		public String handleLoginRequest(@RequestParam String name,@RequestParam String password, ModelMap model){
+			if(!service.validateUser(name, password)){
+				model.put("errorMessage", "Invalid credntials. Please enter valid username and password!");
+				return "login";
+			}
+				model.put("name", name);
+				model.put("password", password);
+				return "welcome";
+		}
+}
